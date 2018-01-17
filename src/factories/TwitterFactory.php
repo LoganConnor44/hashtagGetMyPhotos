909a8f0f1b85695aebc6ccbe38e93281a8dc4da9
Logan Connor <loganconnor44@gmail.com>
2018-01-17 10:38:20 -0500

    updated docblock

--- src/factories/TwitterFactory.php
+++ src/factories/TwitterFactory.php
@@ -4,11 +4,14 @@ namespace HashTagGetMyPhotos\Factory;
 use HashTagGetMyPhotos\Products\SocialMedia;
 use HashTagGetMyPhotos\Products\Twitter;
 
+/**
+ * A factory class to create a Twitter product/object.
+ */
 class TwitterFactory extends AbstractFactory {
 
 	/**
 	 * Calls the AbstractFactory constructor to verify that shape is valid
-	 * @param array $config
+	 * @param array $config Type of platform to create.
 	 * @return void
 	 */
 	public function __construct(array $config) {
@@ -16,6 +19,10 @@ class TwitterFactory extends AbstractFactory {
 		parent::__construct($nameOfPlatform);
 	}
 	
+	/**
+	 * Returns a Twitter object.
+	 * @return Twitter
+	 */
 	public static function getPlatform() : SocialMedia {
 			return new Twitter();
 	}
